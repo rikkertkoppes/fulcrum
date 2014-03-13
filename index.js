ae36de019e90c3129a90f3012940c46ac3bfa35e
rikkertkoppes <rikkert.koppes@theapsgroup.nl>
2014-03-13 12:06:23 +0100

    Added UMD definition pattern from https://github.com/umdjs/umd/blob/master/returnExports.js

--- index.js
+++ index.js
@@ -94,4 +94,24 @@ pivot.max = function(field) {
     };
 };
 
-module.exports = pivot;
+//https://github.com/umdjs/umd/blob/master/returnExports.js
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.pivot = factory();
+  }
+}(this, function () {
+
+    // Just return a value to define the module export.
+    // This example returns an object, but the module
+    // can return a function as the exported value.
+    return pivot;
+}));
